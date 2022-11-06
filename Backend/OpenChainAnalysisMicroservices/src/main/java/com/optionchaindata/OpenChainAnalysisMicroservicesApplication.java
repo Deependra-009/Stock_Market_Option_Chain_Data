b7921a205719d02b9ce0c105ac22c6b98fb04ac8
Deependra-009 <deependra04trivedi@gmail.com>
2022-11-07 01:40:58 +0530

    backend

---
+++ Backend/OpenChainAnalysisMicroservices/src/main/java/com/optionchaindata/OpenChainAnalysisMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.optionchaindata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenChainAnalysisMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenChainAnalysisMicroservicesApplication.class, args);
+	}
+
+}
---
+++ Backend/OpenChainAnalysisMicroservices/src/test/java/com/optionchaindata/OpenChainAnalysisMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.optionchaindata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenChainAnalysisMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
